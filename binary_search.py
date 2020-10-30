3ad23e14144545d30f6d86bbe11a8f59e3bcfb7f
adamstok <adamstokowski@outlook.com>
2020-10-30 10:19:57 +0100

    binary search for the last element

--- binary_search.py
+++ binary_search.py
@@ -66,8 +66,30 @@ search_left(sorted_dups, 8)
 search_left(sorted_dups, 788)
 
 
+@runtime
+def search_right(arr,el):
+    l = 0
+    r = len(arr)
+    count = 0
+    while l < r:
+        m = (l+r) // 2
+        if arr[m] > el :
+            count += 1
+            print(f'{count}: {arr[m]} > {el}')
+            r = m
+        else:
+            count += 1
+            print(f'{count}: {arr[m]} <= {el}')
+            l = m + 1
+    if arr[r-1] == el:
+        print(f'{el} found on index {r-1}')
+        return r-1
+    elif l > r:
+        print(f'{el} not found !')
 
 
-
-
+sorted_dups = [2, 2, 2, 3, 8, 8, 45, 67, 123, 123, 123, 345, 452, 652, 788, 788, 788, 876, 4566, 76899, 88665, 5464545645]
+search_right(sorted_dups, 2)
+search_right(sorted_dups, 8)
+search_right(sorted_dups, 788)
 
