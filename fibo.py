c26705d93545338798fe6a3ecbf80f4ce7bdef6a
adamstok <adamstokowski@outlook.com>
2020-12-09 13:46:29 +0100

    the winner: fibo3

--- fibo.py
+++ fibo.py
@@ -24,4 +24,38 @@ def fibo1(n):
         return fibo1(n - 1) + fibo1(n - 2)
 
 
-fibo1(500)  # -> maximum recursion depth exceeded
+@timerun
+def fibo2(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        memo[n] = fibo2(n - 1) + fibo2(n - 2)
+        return memo[n]
+
+
+@timerun
+def fibo3(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        counter = 1
+        val = [0, 1]
+        while counter < n:
+            next_val = val[0] + val[1]
+            val[0] = val[1]
+            val[1] = next_val
+            counter += 1
+        return val[1]
+
+
+print(fibo1(500))  # -> maximum recursion depth exceeded
+print("-----------------")
+print(fibo2(500))  # -> maximum recursion depth exceeded
+print("-----------------")
+print(fibo3(500))  # -> no problem :)
