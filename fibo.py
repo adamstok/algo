a9da5f51f26b1e7d248ab33fce35c8bff66fa189
adamstok <adamstokowski@outlook.com>
2020-12-09 13:30:45 +0100

    max depth exceeded

--- fibo.py
+++ fibo.py
@@ -6,9 +6,22 @@ import time
 def timerun(f):
     def wrap(*args):
         t1 = time.time()
-        a = f()
+        a = f(*args)
         t2 = time.time()
-        print(f"runtime: {round(t2 - t1, 4)}")
+        print(f"runtime: {round(t2 - t1, 4)} sec")
         return a
 
     return wrap
+
+
+@timerun
+def fibo1(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fibo1(n - 1) + fibo1(n - 2)
+
+
+fibo1(500)  # -> maximum recursion depth exceeded
